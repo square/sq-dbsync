d47313b20a1674c7a255edf7afb2951bab6bc105
Xavier Shay <xavier@squareup.com>
2012-12-17 16:59:06 +1100

    Unit changes.

--- spec/unit/stream_logger_spec.rb
+++ spec/unit/stream_logger_spec.rb
@@ -18,6 +18,14 @@ describe SQD::Loggers::Stream do
     buffer.should include('failed')
   end
 
+  it 'logs error message when exception is raised' do
+    lambda {
+      logger.measure(:fail) { raise("oh no") }
+    }.should raise_error("oh no")
+    buffer.should include('oh no')
+  end
+
+
   it 'logs specified strings' do
     logger.log('logging is good')
     buffer.should include('logging is good')
